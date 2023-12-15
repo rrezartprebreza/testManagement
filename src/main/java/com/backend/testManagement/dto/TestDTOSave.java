ace85564556644d9b08b285abd7fa995142e1ee7
notebook <rrezart.prebreza@gmail.com>
2023-12-15 11:08:43 +0100

    added pipline for project

--- src/main/java/com/backend/testManagement/dto/TestDTOSave.java
+++ src/main/java/com/backend/testManagement/dto/TestDTOSave.java
@@ -1,9 +1,7 @@
 package com.backend.testManagement.dto;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,6 +9,8 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TestDTOSave {
 
     @NotBlank
